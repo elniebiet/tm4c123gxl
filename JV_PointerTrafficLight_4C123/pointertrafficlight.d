4e06cff4a980cb97fc2e8a7a1a2578a873f5320d
Aniebiet Akpan <aniebiet.akpan@gmail.com>
2021-03-25 12:10:20 +0000

    rem files

--- JV_PointerTrafficLight_4C123/pointertrafficlight.d
+++
@@ -1,5 +0,0 @@
-.\pointertrafficlight.o: PointerTrafficLight.c
-.\pointertrafficlight.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\pointertrafficlight.o: PLL.h
-.\pointertrafficlight.o: SysTick.h
-.\pointertrafficlight.o: inc/tm4c123gh6pm.h
